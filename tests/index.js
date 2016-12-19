920fc793631a572667a9f0ecba214adc6075cb2a
Jan Blasko <honza@lucien144.net>
2016-12-20 09:25:27 +1100

    Tests for source.
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,9 @@
 const ascii = require('../src/lib.js');
-const asciiObj = new ascii({});
 
 describe('getRandomInt', function () {
 
   it('should be >= 1 && <= 100', function () {
+    const asciiObj = new ascii({});
     const rand = asciiObj.getRandomInt(1, 100);
     expect(rand).toBeGreaterThan(0.9)
     expect(rand).toBeLessThan(100.1);
@@ -14,6 +14,7 @@ describe('getRandomInt', function () {
 describe('prepareQuery', function () {
 
   it('should be "searchType=image&cx="', function () {
+    const asciiObj = new ascii({});
     const defaults = {
       searchType: 'image',
       cx: ''
@@ -23,6 +24,7 @@ describe('prepareQuery', function () {
   });
 
   it('should be "api_key="', function () {
+    const asciiObj = new ascii({});
     const defaults = {
       api_key: ''
     };
@@ -31,8 +33,37 @@ describe('prepareQuery', function () {
   });
 
   it('should be empty', function () {
+    const asciiObj = new ascii({});
     const query = asciiObj.prepareQuery({});
     expect(query).toEqual('');
+  });
+
+});
+
+describe('checking this.source', function () {
+
+  it('should be clipart', function () {
+    const asciiObj = new ascii({});
+    const source = asciiObj.source;
+    expect(source).toEqual('clipart');
+  });
+
+  it('should be google', function () {
+    const asciiObj = new ascii({source: 'google'});
+    const source = asciiObj.source;
+    expect(source).toEqual('google');
+  });
+
+  it('should be flickr', function () {
+    const asciiObj = new ascii({source: 'flickr'});
+    const source = asciiObj.source;
+    expect(source).toEqual('flickr');
+  });
+
+  it('should be clipart for unknown source', function () {
+    const asciiObj = new ascii({source: 'bing'});
+    const source = asciiObj.source;
+    expect(source).toEqual('clipart');
   });
 
 });
